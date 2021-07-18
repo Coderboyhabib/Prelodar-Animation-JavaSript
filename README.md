64aefade76faa06a91b28ad2d37395d558e03312
Coderboyhabib <Developer.habibur@gmail.com>
2021-07-18 18:25:54 +0600

    Prelodar Animation!

---
+++ README.md
@@ -0,0 +1,1 @@
+# Prelodar-Animation-JavaSript
---
+++ app.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', () => {
+    const preload = document.querySelector('.prelod');
+    preload.classList.add('prelod_end');
+});
